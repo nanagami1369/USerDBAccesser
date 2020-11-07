0bf8611f6761eb07c95df8ec7d330312acea43c3
nanagami1369 <nanagami1369@gmail.com>
2020-11-08 01:12:44 +0900

    テストを追加

---
+++ tests/user_test.cpp
@@ -0,0 +1,8 @@
+#include "User.h"
+#include <gtest/gtest-matchers.h>
+#include <gtest/gtest.h>
+
+TEST(UserTest, toString) {
+    auto user = new User("1", "山田 太郎", "12345678", true, ADMIN);
+    ASSERT_EQ("1|山田 太郎|********|true|ADMIN", user->toString());
+}
