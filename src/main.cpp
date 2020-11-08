c183b07d28b6c142af22e4090ccb2f6aaaacb89a
nanagami1369 <nanagami1369@gmail.com>
2020-11-09 08:52:07 +0900

    DBの切り替えをcmakeでの切り替えから、main関数内での切り替えに変更

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
 #include "Controller.h"
+#include "MemoryUserDB.h"
+#include "SQLiteUserDB.h"
 #include "User.h"
 #include "UserDB.h"
 int main(int argc, char const *argv[]) {
-    auto db = new UserDB();
+    UserDB *db;
+    //コンパイラーからのマクロ定義で動作を切り替える
+    if (DB_TYPE == "memory") {
+        db = new MemoryUserDB();
+
+    } else if (DB_TYPE == "sqlite3") {
+        db = new SQLiteUserDB();
+    }
     auto controller = new Controller(db);
     controller->start();
     delete controller;
