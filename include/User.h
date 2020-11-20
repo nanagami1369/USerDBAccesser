abe5e2f5395272f6a3af7100182faaa2fbf67436
nanagami1369 <nanagami1369@gmail.com>
2020-11-20 17:25:45 +0900

    user.hにコメントを追加

--- include/User.h
+++ include/User.h
@@ -1,12 +1,25 @@
+/**
+ * @file User.h
+ * @brief ユーザーアカウントを保持するクラス
+ */
 #pragma once
-#include <string>
 #include "t_level.h"
+#include <string>
+/**
+ * @brief ユーザーアカウントを保持するクラス
+ * @attention このクラスではデータを保持するだけなので別途バリデーション等をすること
+ */
 class User {
   public:
+    /** @brief 会員番号 */
     uint ID;
+    /** @brief アカウント名 */
     std::string Name;
+    /** @brief パスワード */
     std::string Pass;
+    /** @brief アカウントの状態 (有効、無効) */
     bool Avail;
+    /** @brief 権限 */
     t_Level Level;
     User(uint id,
          std::string name,
