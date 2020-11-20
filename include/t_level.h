41b828a298c28ae3841c701651558524103e4f22
nanagami1369 <nanagami1369@gmail.com>
2020-11-20 17:13:25 +0900

    t_level.hにコメントを追加

--- include/t_level.h
+++ include/t_level.h
@@ -1,11 +1,24 @@
+/**
+ * @file t_level.h
+ * @brief ユーザーアカウントの権限を定義
+ */
 #pragma once
 #include <string>
+/**
+ * @brief ユーザーアカウントの権限
+ */
 enum t_Level {
     ADMIN,
     PREM,
     GEN,
     TRY
 };
-
+/**
+ * @brief 権限から文字列を生成
+ */
 std::string t_LevelToString(t_Level level);
+/**
+ * @brief 数字を権限へ変換
+ * @attention 権限の数より大きな値が入力された場合 invalid_argument を出す
+ */
 t_Level intToT_Level(int level);
