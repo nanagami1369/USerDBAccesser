10639f34e85472c2928ee48af506c20aa544cc15
nanagami1369 <nanagami1369@gmail.com>
2020-11-21 12:01:36 +0900

    ValidationException.hにコメントを追加

--- include/ValidationException.h
+++ include/ValidationException.h
@@ -1,4 +1,11 @@
+/**
+ * @file ValidationException.h
+ * @brief バリデーションエラーを管理するクラス
+ */
 #include <stdexcept>
+/**
+ * バリデーションエラーを管理するクラス
+ */
 class ValidationException : public std::runtime_error {
   public:
     ValidationException(const char *_Message);
