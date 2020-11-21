35092a2960f780e7de64806e18a4e8bc76e4bba6
nanagami1369 <nanagami1369@gmail.com>
2020-11-21 11:58:16 +0900

    Controller.hにコメントを追加

--- include/Controller.h
+++ include/Controller.h
@@ -1,24 +1,65 @@
+/**
+ * @file Controller.h
+ * @brief UserDBをCLIで扱うためのクラス
+ */
 #pragma once
 #include "UserDB.h"
 #include <string>
+/**
+ * @file Controller.h
+ * @brief UserDBをCLIから扱うコントローラー
+ */
 class Controller {
   private:
     UserDB *db;
 
+    /**
+     * @brief ユーザーIDを入力させるプロンプト
+     */
     uint InputIdPrompt();
-
+    /**
+     * @brief ユーザーアカウントを追加する処理
+     */
     void addUser();
+    /**
+     * @brief ユーザーアカウントを追加する処理
+     */
     void removeUser();
+    /**
+     * @brief ユーザーアカウントの状態を変更する処理
+     */
     void changeAvail();
+    /**
+     * @brief ユーザーアカウントを検索する処理
+     */
     void searchUser();
 
+    /**
+     * @brief ユーザーアカウントをIdで検索する処理
+     * @note searchUser関数から呼ばれる
+     */
     void searchUserById();
+    /**
+     * @brief ユーザーアカウントを名前で検索する処理
+     * @note searchUser関数から呼ばれる
+     */
     void searchUserByName();
+    /**
+     * @brief ユーザーアカウントを状態で検索する処理
+     * @note searchUser関数から呼ばれる
+     */
     void searchUserByAvail();
+    /**
+     * @brief ユーザーアカウントを権限で検索する処理
+     * @note searchUser関数から呼ばれる
+     */
     void searchUserByLevel();
 
   public:
     Controller(UserDB *db);
     ~Controller();
+    /**
+     * @brief コントローラーを起動する。
+     */
     void start();
 };
