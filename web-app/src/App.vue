89cef25a8af86c9ba9a7ad29caa8ff677e053d05
nanagami1369 <nanagami1369@gmail.com>
2020-12-25 16:44:54 +0900

    コントローラー用のボタンのスタイルを作成

--- web-app/src/App.vue
+++ web-app/src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="app">
     <header id="main-header" class="content">
-      <h1>{{ title }}</h1>
+      <h1 id="header-title">{{ title }}</h1>
+      <div id="header-button-aria">
+        <button type="button" class="header-button">アカウントを追加</button>
+        <button type="button" class="header-button">更新</button>
+        <button type="button" class="header-button remove-button">削除</button>
+      </div>
     </header>
     <main>
       <AddUserForm @submit="sendFormAddUser" />
@@ -47,6 +52,53 @@ export default class App extends Vue {
 * {
   margin: 0;
   padding: 0;
+}
+
+#main-header {
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  align-items: center;
+}
+
+#header-title {
+  flex-grow: 1;
+  font-size: 2em;
+  flex-shrink: 0;
+}
+
+#header-button-aria {
+  flex-shrink: 0;
+  flex-grow: 3;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: flex-end;
+}
+
+.header-button {
+  flex-shrink: 0;
+  font-size: 1.4em;
+  padding: 0.2em 0.8em;
+  margin: 0px 5px;
+  color: #ffffff;
+  background-color: #4472c4;
+  border-color: #2f528f;
+  border-radius: 5px;
+}
+
+.header-button:active {
+  background-color: #5b81c4;
+  border-color: #4472c4;
+}
+
+.remove-button {
+  background-color: #ed7d31;
+  border-color: #ae5a21;
+}
+
+.remove-button:active {
+  border-color: #ed7d31;
+  background-color: #eb965e;
 }
 
 .content {
